b9821da18a432ab235707412b625ebe0982df812
Melissa Sperry <sperry.mel@gmail.com>
2020-03-04 03:38:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,20 @@
 class Dog
-attr_accessor :name
+  attr_accessor :name
 
-@@all = []
+  @@all = []
 
-def initialize(name)
-  @name = name
-  @@all << self
-end
-
-def self.all
-  @@all.each do |dog|
-    puts dog.name
+  def initialize(name)
+    @name = name
+    @@all << self
   end
-end
 
-def Dog.clear_all
-  @@all.clear
-end
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.all
+    @@all.each do |d|
+      puts d.name
+    end
+  end
 end
